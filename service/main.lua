2ef12324c2ba949a4736b07fc4cc817fb79ef314
dawnwizard <948507867@qq.com>
2018-02-06 15:16:32 +0800

    0.08

--- service/main.lua
+++ service/main.lua
@@ -2,6 +2,8 @@ local skynet = require "skynet"
 
 skynet.start(function()
 	skynet.error("Server start")
+	skynet.newservice("mgr_server")
+
 	-- if not skynet.getenv "daemon" then
 	-- 	local console = skynet.newservice("console")
 	-- end
@@ -13,5 +15,5 @@ skynet.start(function()
 	-- })
 	-- local hub = skynet.uniqueservice "hub"
 	-- skynet.call(hub, "lua", "open", "0.0.0.0", 5678)
-	-- skynet.exit()
+	skynet.exit()
 end)
