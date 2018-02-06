7720889be95d192b3ff283df09eda136621ddff6
dawnwizard <948507867@qq.com>
2018-02-06 14:17:43 +0800

    0.04

--- run.sh
+++ run.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
-export ROOT=$(cd `dirname $0`; pwd)
+# export ROOT=$(cd `dirname $0`; pwd)
+export ROOT = "."
 export DAEMON=false
 
-while getopts "Dk" arg
-do
-	case $arg in
-		D)
-			export DAEMON=true
-			;;
-		k)
-			kill `cat $ROOT/run/skynet.pid`
-			exit 0;
-			;;
-	esac
-done
+# while getopts "Dk" arg
+# do
+# 	case $arg in
+# 		D)
+# 			export DAEMON=true
+# 			;;
+# 		k)
+# 			kill `cat $ROOT/run/skynet.pid`
+# 			exit 0;
+# 			;;
+# 	esac
+# done
 
 $ROOT/skynet/skynet $ROOT/config
 
