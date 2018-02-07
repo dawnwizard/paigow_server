33a018455a38560ddc091be5ce5a5eeedec58dbe
dawnwizard <948507867@qq.com>
2018-02-07 12:05:16 +0800

    1.13

--- service/mgr_server.lua
+++ service/mgr_server.lua
@@ -51,7 +51,7 @@ local function handle_socket(id)
 end
 
 skynet.start(function()
-    local address = "0.0.0.0:8001"
+    local address = "0.0.0.0:8080"
     skynet.error("Listening "..address)
     local id = assert(socket.listen(address))
     socket.start(id , function(id, addr)
