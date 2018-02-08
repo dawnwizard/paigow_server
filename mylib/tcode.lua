b536e8c3c9111cd9162f3730463b3950a6117942
dawnwizard <948507867@qq.com>
2018-02-08 16:51:49 +0800

    1.21

--- mylib/tcode.lua
+++ mylib/tcode.lua
@@ -4,6 +4,7 @@ local tcode = {}
 function tcode:decode( s_msg )
 	local t_msg = {}
 	local idx_id1, idx_id2 = string.find(s_msg,"$|")
+	print("idx_id1:", idx_id1, "idx_id2:", idx_id2)
 	if idx_id1 and idx_id2 then
 		t_msg.user_id = tonumber(string.sub(s_msg, 1, idx_id1 -1))
 		t_msg.msg_type = tonumber(string.sub(s_msg, idx_id2 + 1, idx_id2 + 2))
